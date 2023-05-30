64e23a66fcfb2532b3dd7b50c2ffb439d103702b
Weili Hu s320116 <s320116@studenti.polito.it>
2023-05-30 14:24:38 +0200

    modified comments on controller.js

--- code/controllers/controller.js
+++ code/controllers/controller.js
@@ -3,7 +3,7 @@ import { Group, User } from "../models/User.js";
 import { handleDateFilterParams, handleAmountFilterParams, verifyAuth, checkMissingOrEmptyParams} from "./utils.js";
 
 /**
- * Create a new category
+ * Create a new category  Done
   - Request Parameters: None
   - Request Body Content: An object having attributes `type` and `color`
   - Response `data` Content: An object having attributes `type` and `color`
@@ -19,6 +19,7 @@ import { handleDateFilterParams, handleAmountFilterParams, verifyAuth, checkMiss
             return res.status(401).json({ error: adminAuth.cause }) 
 
         const { type, color } = req.body;
+
         //Check for missing or empty string parameter
         if(checkMissingOrEmptyParams([type, color], res))
             return res.status(400).json({ error: "missing parameters" });
@@ -40,7 +41,7 @@ import { handleDateFilterParams, handleAmountFilterParams, verifyAuth, checkMiss
 
 
 /**
- * Edit a category's type or color
+ * Edit a category's type or color  Done
   - Request Parameters: A string equal to the type of the category that must be edited
   - Request Body Content: An object having attributes `type` and `color` equal to the new values to assign to the category
   - Response `data` Content: An object with parameter `message` that confirms successful editing and a parameter `count` that is equal to the count of transactions whose category was changed with the new type
@@ -58,6 +59,7 @@ export const updateCategory = async (req, res) => {
             return res.status(401).json({ error: adminAuth.cause }) 
 
         const { type, color } = req.body;
+
         //Check for missing or empty string parameter
         if(checkMissingOrEmptyParams([type, color], res))
             return res.status(400).json({ error: "missing parameters" });
@@ -95,7 +97,7 @@ export const updateCategory = async (req, res) => {
 }
 
 /**
- * Delete a category
+ * Delete a categories  Done
   - Request Parameters: None
   - Request Body Content: An array of strings that lists the `types` of the categories to be deleted
   - Response `data` Content: An object with parameter `message` that confirms successful deletion and a parameter `count` that is equal to the count of affected transactions (deleting a category sets all transactions with that category to have `investment` as their new category)
@@ -125,14 +127,14 @@ export const deleteCategory = async (req, res) => {
         for(let i=0 ; i<typeListLength ; i++){
             const foundCategory = await categories.findOne({ type: typeList[i] });
             if(!foundCategory){
-                return res.status(401).json({ error: "Category for type '" + typeList[i] + "' not found" }); //Category with specified type not found
+                return res.status(400).json({ error: "Category for type '" + typeList[i] + "' not found" }); //Category with specified type not found
             }
         }
 
         //count the tot number of categories
         const totNumberCategories = (await categories.find({})).length;
-        //Only one category in db, no deletion done
-        if(totNumberCategories == 1)
+        //If only one category in db, no deletion done
+        if(totNumberCategories === 1)
             return res.status(400).json({ error: "Only one category remaining in database" });
 
         let updateResult;
@@ -166,21 +168,22 @@ export const deleteCategory = async (req, res) => {
 }
 
 /**
- * Return all the categories
+ * Return all the categories  Done
+  - Request Parameters: None  
   - Request Body Content: None
   - Response `data` Content: An array of objects, each one having attributes `type` and `color`
   - Optional behavior:
     - empty array is returned if there are no categories
+  - Returns a 401 error if called by a user who is not authenticated (authType = Simple)
  */
 export const getCategories = async (req, res) => {
     try {
         const simpleAuth = verifyAuth(req, res, { authType: "Simple" })
 
-        if(!simpleAuth.authorized){
+        if(!simpleAuth.authorized)
             return res.status(401).json({ error: simpleAuth.cause }) 
-        }
 
-        let data = await categories.find({})
+        let data = await categories.find({})  
 
         let categoriesData = data.map(v => Object.assign({}, { type: v.type, color: v.color }))
         res.locals.refreshedTokenMessage = "Access token has been refreshed. Remember to copy the new one in the headers of subsequent calls;" //tocheck
@@ -191,32 +194,53 @@ export const getCategories = async (req, res) => {
 }
 
 /**
- * Create a new transaction made by a specific user
+ * Create a new transaction made by a specific user  Done
+  - Request Parameters: A string equal to the `username` of the involved user
   - Request Body Content: An object having attributes `username`, `type` and `amount`
   - Response `data` Content: An object having attributes `username`, `type`, `amount` and `date`
-  - Optional behavior:
-    - error 401 is returned if the username or the type of category does not exist
+  - Returns a 400 error if the request body does not contain all the necessary attributes 
+  - Returns a 400 error if at least one of the parameters in the request body is an empty string 
+  - Returns a 400 error if the type of category passed in the request body does not represent a category in the database 
+  - Returns a 400 error if the username passed in the request body is not equal to the one passed as a route parameter 
+  - Returns a 400 error if the username passed in the request body does not represent a user in the database 
+  - Returns a 400 error if the username passed as a route parameter does not represent a user in the database 
+  - Returns a 400 error if the amount passed in the request body cannot be parsed as a floating value (negative numbers are accepted) 
+  - Returns a 401 error if called by an authenticated user who is not the same user as the one in the route parameter (authType = User)
  */
 export const createTransaction = async (req, res) => {
     try {
-
-        //TODO: check params with weili function
-
         const userAuth = verifyAuth(req, res, { authType: "Simple"});
 
         if(!userAuth.authorized)
-        {
             return res.status(401).json({ error: "user: " + userAuth.cause }) 
-        }
+
         const { username, amount, type } = req.body;
 
+        //Check for missing or empty string parameter
+        if(checkMissingOrEmptyParams([username, amount, type], res))
+            return res;
+
+        //Try parsing amount as float
+        if(isNan(parseFloat(amount)))
+            return res.status(400).json({ error: "Invalid amount value" });
+
+        //Check if body username is the same as the one in route
+        if( req.params.username !== username )
+            return res.status(400).json({ error: "Username mismatch" });
+
+        //Search for user and category(body)
         const matchedUser = await User.findOne({ username: username });
         if(!matchedUser) {
-            return res.status(401).json({ error: "the user does not exist" })
+            return res.status(400).json({ error: "The user does not exist" })
         }
         const matchedCategory = await categories.findOne({ type: type });
         if(!matchedCategory) {
-            return res.status(401).json({ error: "the category does not exist" })
+            return res.status(400).json({ error: "The category does not exist" })
+        }
+        //Search for requesting user(route)
+        const matchedUserRoute = await User.findOne({ username: req.param.username });
+        if(!matchedUserRoute) {
+            return res.status(400).json({ error: "The requesting user does not exist" })
         }
 
         const new_transactions = new transactions({ username, amount, type });
@@ -230,14 +254,15 @@ export const createTransaction = async (req, res) => {
 
 /**
  * Return all transactions made by all users
+  - Request Parameters: None
   - Request Body Content: None
   - Response `data` Content: An array of objects, each one having attributes `username`, `type`, `amount`, `date` and `color`
   - Optional behavior:
     - empty array must be returned if there are no transactions
+  - Returns a 401 error if called by an authenticated user who is not an admin (authType = Admin)
  */
 export const getAllTransactions = async (req, res) => {
     try {
-
         const adminAuth = verifyAuth(req, res, { authType: "Admin" })
         if (!adminAuth.authorized)
             return res.status(401).json({ error:  " admin: " + adminAuth.cause }) 
@@ -268,13 +293,17 @@ export const getAllTransactions = async (req, res) => {
 
 /**
  * Return all transactions made by a specific user
+  - Request Parameters: A string equal to the `username` of the involved user
   - Request Body Content: None
   - Response `data` Content: An array of objects, each one having attributes `username`, `type`, `amount`, `date` and `color`
   - Optional behavior:
-    - error 401 is returned if the user does not exist
     - empty array is returned if there are no transactions made by the user
     - if there are query parameters and the function has been called by a Regular user then the returned transactions must be filtered according to the query parameters
- */
+  - Returns a 400 error if the username passed as a route parameter does not represent a user in the database
+  - Returns a 401 error if called by an authenticated user who is not the same user as the one in the route (authType = User) if the route is `/api/users/:username/transactions`
+  - Returns a 401 error if called by an authenticated user who is not an admin (authType = Admin) if the route is `/api/transactions/users/:username`
+  - Can be filtered by date and amount if the necessary query parameters are present and if the route is `/api/users/:username/transactions`
+    */
 export const getTransactionsByUser = async (req, res) => {
     try {
         //Distinction between route accessed by Admins or Regular users for functions that can be called by both
@@ -289,7 +318,7 @@ export const getTransactionsByUser = async (req, res) => {
                 const username = req.params.username;
                 const matchedUser = await User.findOne({ username: username });
                 if(!matchedUser) {
-                    return res.status(401).json({ error: "the user does not exist" })
+                    return res.status(400).json({ error: "the user does not exist" })
                 }
                 
                 //Query the MONGODB Transactions
@@ -325,7 +354,7 @@ export const getTransactionsByUser = async (req, res) => {
             const username = req.params.username;
             const matchedUser = await User.findOne({ username: username });
             if(!matchedUser) {
-                return res.status(401).json({ error: "the user does not exist" })
+                return res.status(400).json({ error: "the user does not exist" })
             }
             
             const queryDate   = handleDateFilterParams(req);
@@ -367,11 +396,15 @@ export const getTransactionsByUser = async (req, res) => {
 
 /**
  * Return all transactions made by a specific user filtered by a specific category
+  - Request Parameters: A string equal to the `username` of the involved user, a string equal to the requested `category`
   - Request Body Content: None
   - Response `data` Content: An array of objects, each one having attributes `username`, `type`, `amount`, `date` and `color`, filtered so that `type` is the same for all objects
   - Optional behavior:
     - empty array is returned if there are no transactions made by the user with the specified category
-    - error 401 is returned if the user or the category does not exist
+  - Returns a 400 error if the username passed as a route parameter does not represent a user in the database
+  - Returns a 400 error if the category passed as a route parameter does not represent a category in the database
+  - Returns a 401 error if called by an authenticated user who is not the same user as the one in the route (authType = User) if the route is `/api/users/:username/transactions/category/:category`
+  - Returns a 401 error if called by an authenticated user who is not an admin (authType = Admin) if the route is `/api/transactions/users/:username/category/:category`
  */
 export const getTransactionsByUserByCategory = async (req, res) => {
     try {
@@ -379,18 +412,18 @@ export const getTransactionsByUserByCategory = async (req, res) => {
         if (req.url.indexOf("/transactions/users/") >= 0) {   //admin 
                     const adminAuth = verifyAuth(req, res, { authType: "Admin" })
                     if (!adminAuth.authorized)
-                        return res.status(401).json({ error: " admin: " + adminAuth.cause }) 
+                        return res.status(401).json({ error: "Admin: " + adminAuth.cause }) 
                     //Search requested user
                 const username = req.params.username;
                 const matchedUserid = await User.findOne({username: username });
                 if(!matchedUserid) {
-                    return res.status(401).json({ error :"the user does not exist" });
+                    return res.status(400).json({ error : "The user does not exist" });
                 }
                 //Search requested category
                 const category = req.params.category;
                 const matchedCategory = await categories.findOne({ type: category });
                 if(!matchedCategory) {
-                    return res.status(401).json({ error: "the category does not exist" });
+                    return res.status(400).json({ error: "The category does not exist" });
                 }
                 transactions.aggregate([
                     { $match: { username: username, type: category }},
@@ -414,19 +447,19 @@ export const getTransactionsByUserByCategory = async (req, res) => {
             const userAuth = verifyAuth(req, res, { authType: "User", username: req.params.username })
             if(!userAuth.authorized)
             {
-                return res.status(401).json({ error: "user: " + userAuth.cause }) 
+                return res.status(401).json({ error: "User: " + userAuth.cause }) 
             }
              //Search requested user
         const username = req.params.username;
         const matchedUserid = await User.findOne({username: username });
         if(!matchedUserid) {
-            return res.status(401).json({ error :"the user does not exist" });
+            return res.status(400).json({ error : "The user does not exist" });
         }
         //Search requested category
         const category = req.params.category;
         const matchedCategory = await categories.findOne({ type: category });
         if(!matchedCategory) {
-            return res.status(401).json({ error: "the category does not exist" });
+            return res.status(400).json({ error: "The category does not exist" });
         }
 
         transactions.aggregate([
@@ -454,11 +487,14 @@ export const getTransactionsByUserByCategory = async (req, res) => {
 
 /**
  * Return all transactions made by members of a specific group
+  - Request Parameters: A string equal to the `name` of the requested group
   - Request Body Content: None
   - Response `data` Content: An array of objects, each one having attributes `username`, `type`, `amount`, `date` and `color`
   - Optional behavior:
-    - error 401 is returned if the group does not exist
     - empty array must be returned if there are no transactions made by the group
+  - Returns a 400 error if the group name passed as a route parameter does not represent a group in the database
+  - Returns a 401 error if called by an authenticated user who is not part of the group (authType = Group) if the route is `/api/groups/:name/transactions`
+  - Returns a 401 error if called by an authenticated user who is not an admin (authType = Admin) if the route is `/api/transactions/groups/:name`
  */
 export const getTransactionsByGroup = async (req, res) => {
     try {
@@ -548,11 +584,15 @@ export const getTransactionsByGroup = async (req, res) => {
 
 /**
  * Return all transactions made by members of a specific group filtered by a specific category
+  - Request Parameters: A string equal to the `name` of the requested group, a string equal to the requested `category`
   - Request Body Content: None
   - Response `data` Content: An array of objects, each one having attributes `username`, `type`, `amount`, `date` and `color`, filtered so that `type` is the same for all objects.
   - Optional behavior:
-    - error 401 is returned if the group or the category does not exist
     - empty array must be returned if there are no transactions made by the group with the specified category
+  - Returns a 400 error if the group name passed as a route parameter does not represent a group in the database
+  - Returns a 400 error if the category passed as a route parameter does not represent a category in the database
+  - Returns a 401 error if called by an authenticated user who is not part of the group (authType = Group) if the route is `/api/groups/:name/transactions/category/:category`
+  - Returns a 401 error if called by an authenticated user who is not an admin (authType = Admin) if the route is `/api/transactions/groups/:name/category/:category`
  */
 export const getTransactionsByGroupByCategory = async (req, res) => {
     try {
@@ -648,10 +688,13 @@ export const getTransactionsByGroupByCategory = async (req, res) => {
 
 /**
  * Delete a transaction made by a specific user
+  - Request Parameters: A string equal to the `username` of the involved user
   - Request Body Content: The `_id` of the transaction to be deleted
   - Response `data` Content: A string indicating successful deletion of the transaction
-  - Optional behavior:
-    - error 401 is returned if the user or the transaction does not exist
+  - Returns a 400 error if the request body does not contain all the necessary attributes
+  - Returns a 400 error if the username passed as a route parameter does not represent a user in the database
+  - Returns a 400 error if the `_id` in the request body does not represent a transaction in the database
+  - Returns a 401 error if called by an authenticated user who is not the same user as the one in the route (authType = User)
  */  //a regular user should be capable deleting only own transactions
 export const deleteTransaction = async (req, res) => {
     try {
@@ -687,10 +730,14 @@ export const deleteTransaction = async (req, res) => {
 
 /**
  * Delete multiple transactions identified by their ids
+  - Request Parameters: None  
   - Request Body Content: An array of strings that lists the `_ids` of the transactions to be deleted
   - Response `data` Content: A message confirming successful deletion
-  - Optional behavior:
-    - error 401 is returned if at least one of the `_ids` does not have a corresponding transaction. Transactions that have an id are not deleted in this case
+  - In case any of the following errors apply then no transaction is deleted
+  - Returns a 400 error if the request body does not contain all the necessary attributes
+  - Returns a 400 error if at least one of the ids in the array is an empty string
+  - Returns a 400 error if at least one of the ids in the array does not represent a transaction in the database
+  - Returns a 401 error if called by an authenticated user who is not an admin (authType = Admin)
  */
 export const deleteTransactions = async (req, res) => {
     try {

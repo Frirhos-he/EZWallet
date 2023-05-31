5bc612a19c215713c57f3775a5e0dd28cc3c1b85
Seyed Alireza Khalilinejad <s308461@studenti.polito.it>
2023-05-31 18:32:51 +0200

    update createcategory test case a

--- code/test/controller.unit.test.js
+++ code/test/controller.unit.test.js
@@ -18,6 +18,67 @@ describe("createCategory", () => {
         expect(true).toBe(true);
     });
 })
+---------------------------------------------------------------------------
+import request from 'supertest';
+import { app } from '../app';
+import { categories } from '../models/model';
+
+jest.mock('../models/model');
+
+beforeEach(() => {
+  categories.findOne.mockClear();
+  categories.prototype.save.mockClear();
+});
+
+describe("createCategory", () => {
+  test('should create a new category successfully', async () => {
+    // Mock input data
+    const reqBody = {
+      type: 'testtype',
+      color: 'testcolor',
+    };
+
+    // Mock categories.findOne method to return null (category doesn't exist)
+    categories.findOne.mockResolvedValue(null);
+
+    // Mock categories.prototype.save method
+    categories.prototype.save.mockResolvedValue({
+      type: reqBody.type,
+      color: reqBody.color,
+    });
+
+    // Make the request to create a new category
+    const response = await request(app).post('/create-category').send(reqBody);
+
+    // Check the response
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({
+      data: {
+        type: reqBody.type,
+        color: reqBody.color,
+      },
+    });
+    expect(categories.findOne).toHaveBeenCalledWith({ type: reqBody.type });
+    expect(categories.prototype.save).toHaveBeenCalled();
+  });
+
+  test('should return an error if missing or empty parameters', async () => {
+    // Mock input data with missing or empty parameters
+    const reqBody = {
+      type: '',
+      color: 'testcolor',
+    };
+
+    // Make the request to create a new category
+    const response = await request(app).post('/create-category').send(reqBody);
+
+    // Check the response
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toEqual({ error: 'Missing or empty parameters' });
+    expect(categories.findOne).not.toHaveBeenCalled();
+    expect(categories.prototype.save).not.toHaveBeenCalled();
+  });
+  ---------------------------------------------------------------------------------------------
 
 describe("updateCategory", () => { 
     test('Dummy test, change it', () => {

5ad8a87d0649caeae79eb50968c845ccf1a5df4d
Filippo Greco s309529 <s309529@studenti.polito.it>
2023-06-06 17:12:26 +0200

    CRUD Categories tested

--- code/test/controller.integration.test.js
+++ code/test/controller.integration.test.js
@@ -4,14 +4,23 @@ import { categories, transactions } from "../models/model";
 import { Group, User } from "../models/User";
 import mongoose, { Model } from "mongoose";
 import dotenv from "dotenv";
+import jwt from "jsonwebtoken"
 
 dotenv.config();
 
-const adminToken =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFAaC5pdCIsImlkIjoiNjQ3NjM3MDkxNzQzMTYwY2ZhMzNlMmMwIiwidXNlcm5hbWUiOiJhIiwicm9sZSI6IkFkbWluIiwiaWF0IjoxNjg2MDQ3NzMxLCJleHAiOjE2ODY2NTI1MzF9.zOR658eOOeaH-ixDii4q9m3S-V-tri3STVxgViSPc8s";
+const adminToken = jwt.sign({
+    email: "token@token.token",
+    id: "0",
+    username: "tokenuser",
+    role: "Admin"
+}, process.env.ACCESS_KEY, { expiresIn: '1h' })
 
-const userToken =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InVAaC5pdCIsImlkIjoiNjQ3NWEyOGE2ZjlhZTU5ZmQ0YjZiOTM4IiwidXNlcm5hbWUiOiJVc2VyIiwicm9sZSI6IlJlZ3VsYXIiLCJpYXQiOjE2ODYwNDcwMTYsImV4cCI6MTY4NjA1MDYxNn0.tDNqdUIwwdhPlq6e4dg-Uni9_XcmStDiYMOy1rAGiHo";
+const userToken = jwt.sign({
+    email: "token@token.token",
+    id: "0",
+    username: "tokenuser",
+    role: "Regular"
+}, process.env.ACCESS_KEY, { expiresIn: '1h' })
 
 beforeAll(async () => {
   const dbName = "testingDatabaseController";
@@ -31,19 +40,13 @@ afterAll(async () => {
 
 describe("createCategory", () => { 
       beforeAll(async () => {
-        await User.create({
-            username: "test",
-            email: "test@gmail.com",
-            password: "password",
-            refreshToken: userToken,
-            role: "Regular",
-        });
         await categories.create({
             type: "gym",
             color: "#FFFFFF",
         });
     });
-    test("Nominal scenario", (done) => {
+    
+    test('should create a new category successfully', (done) => {
 
         request(app)
             .post("/api/categories")
@@ -56,18 +59,19 @@ describe("createCategory", () => {
                 `accessToken=${adminToken};refreshToken=${adminToken}`
             )
             .then((response) => {
-                expect(response.status).toBe(200);
                 expect(response.body).toStrictEqual({
                     data: {
                         type: "investment",
                         color: "blue",
                     },
                 });
+                expect(response.status).toBe(200);
                 done();
             })
             .catch((err) => done(err));
     });
-    test("Exception user", (done) => {
+    
+    test("should return an error if an user try to call the api", (done) => {
         request(app)
             .post("/api/categories")
             .send({
@@ -77,11 +81,15 @@ describe("createCategory", () => {
             .set("Cookie", `accessToken=${userToken};refreshToken=${userToken}`)
             .then((response) => {
                 expect(response.status).toBe(401);
+                expect(response.json).toStrictEqual({
+                    error: "Mismatch role"
+                })
                 done();
             })
             .catch((err) => done(err));
     });
-    test("Exception no type", (done) => {
+    
+    test("should return an error if missing field in body", (done) => {
         request(app)
             .post("/api/categories")
             .send({
@@ -100,7 +108,8 @@ describe("createCategory", () => {
             })
             .catch((err) => done(err));
     });
-      test("Exception empty type", (done) => {
+    
+    test("should return an error if a field is an empty string", (done) => {
 
         request(app)
             .post("/api/categories")
@@ -121,7 +130,8 @@ describe("createCategory", () => {
             })
             .catch((err) => done(err));
     });
-        test("Exception category already present", (done) => {
+    
+    test('should return an error if category already exists', (done) => {
         request(app)
             .post("/api/categories")
             .send({
@@ -141,15 +151,33 @@ describe("createCategory", () => {
             })
             .catch((err) => done(err));
     });
-        afterAll(async () => {
+
+    test('should return an error if the tokens are not present', (done) => {
+        
+        request(app)
+            .post("/api/categories")
+            .send({
+                type: "sport",
+                color: "#FFFFFF",
+            })
+            .then((response) => {
+                expect(response.status).toBe(401);
+                expect(response.body).toStrictEqual({
+                    error: "Unauthorized",
+                });
+                done();
+            })
+            .catch((err) => done(err));
+    });
+
+    afterAll(async () => {
         await categories.deleteMany();
-        await User.deleteMany();
     });
 })
 
 describe("updateCategory", () => { 
-        const today = new Date();
-        beforeEach(async () => {
+    const today = new Date();
+    beforeAll(async () => {
         await categories.create({
             type: "investment",
             color: "blue",
@@ -158,21 +186,9 @@ describe("updateCategory", () => {
             type: "work",
             color: "red",
         });
-
-        await User.create({
-            username: "u",
-            email: "u@h.it",
-            password: "p",
-            refreshToken: userToken,
-            role: "Regular",
-        });
-
-        await User.create({
-            username: "a",
-            email: "a@h.it",
-            password: "p",
-            refreshToken: adminToken,
-            role: "Admin",
+        await categories.create({
+            type: "third",
+            color: "yellow",
         });
 
         await transactions.create({
@@ -194,7 +210,8 @@ describe("updateCategory", () => {
             date: today,
         });
     });
-    test("Nominal scenario admin ", (done) => {
+
+    test('should update a category successfully', (done) => {
         request(app)
             .patch("/api/categories/investment")
             .send({
@@ -217,17 +234,367 @@ describe("updateCategory", () => {
             })
             .catch((err) => done(err));
     });
+
+    test('should return an error if missing parameters', (done) => {
+        request(app)
+            .patch("/api/categories/investment")
+            .send({
+                type: "joker",
+            })
+            .set(
+                "Cookie",
+                `accessToken=${adminToken};refreshToken=${adminToken}`
+            )
+            .then((response) => {
+                expect(response.status).toBe(400);
+                expect(response.body).toStrictEqual({
+                    error: "Missing values",
+                });
+                done();
+            })
+            .catch((err) => done(err));
+    });
+
+    test('should return an error if empty strings parameters', (done) => {
+        request(app)
+            .patch("/api/categories/investment")
+            .send({
+                type: "joker",
+                color: "",
+            })
+            .set(
+                "Cookie",
+                `accessToken=${adminToken};refreshToken=${adminToken}`
+            )
+            .then((response) => {
+                expect(response.status).toBe(400);
+                expect(response.body).toStrictEqual({
+                    error: "Empty string values",
+                });
+                done();
+            })
+            .catch((err) => done(err));
+    });
+
+    test('should return an error if category in route parameters doesn\'t exists', (done) => {
+        request(app)
+            .patch("/api/categories/wrong")
+            .send({
+                type: "joker",
+                color: "blue",
+            })
+            .set(
+                "Cookie",
+                `accessToken=${adminToken};refreshToken=${adminToken}`
+            )
+            .then((response) => {
+                expect(response.status).toBe(400);
+                expect(response.body).toStrictEqual({
+                    error: "Category of type 'wrong' not found"
+                });
+                done();
+            })
+            .catch((err) => done(err));
+    });
+
+    test('should return an error if category in body already exists', (done) => {
+        request(app)
+            .patch("/api/categories/work")
+            .send({
+                type: "third",
+                color: "blue",
+            })
+            .set(
+                "Cookie",
+                `accessToken=${adminToken};refreshToken=${adminToken}`
+            )
+            .then((response) => {
+                expect(response.status).toBe(400);
+                expect(response.body).toStrictEqual({
+                    error: "Category of type 'third' already exists",
+                });
+                done();
+            })
+            .catch((err) => done(err));
+    });
+
+    test('should return an error of authentication (user token)', (done) => {
+        request(app)
+            .patch("/api/categories/investment")
+            .send({
+                type: "joker",
+                color: "blue",
+            })
+            .set(
+                "Cookie",
+                `accessToken=${userToken};refreshToken=${userToken}`
+            )
+            .then((response) => {
+                expect(response.status).toBe(401);
+                expect(response.body).toStrictEqual({
+                    error: "Mismatch role"
+                });
+                done();
+            })
+            .catch((err) => done(err));
+    });
+
+    test('should return an error of authentication (missing token)', (done) => {
+        request(app)
+            .patch("/api/categories/investment")
+            .send({
+                type: "joker",
+                color: "blue",
+            })
+            .then((response) => {
+                expect(response.status).toBe(401);
+                expect(response.body).toStrictEqual({
+                    error: "Unauthorized"
+                });
+                done();
+            })
+            .catch((err) => done(err));
+    });
+    
+    afterAll(async () => {
+        await categories.deleteMany();
+        await transactions.deleteMany();
+    });
 })
 
 describe("deleteCategory", () => { 
-    test('Dummy test, change it', () => {
-        expect(true).toBe(true);
+    const today = new Date();
+    beforeAll(async () => {
+        await categories.create({
+            type: "investment",
+            color: "blue",
+        });
+        await categories.create({
+            type: "work",
+            color: "red",
+        });
+
+        await transactions.create({
+            username: "u",
+            amount: 12.54,
+            type: "investment",
+            date: today,
+        });
+        await transactions.create({
+            username: "u",
+            amount: 12.54,
+            type: "work",
+            date: today,
+        });
+        await transactions.create({
+            username: "a",
+            amount: 12.54,
+            type: "work",
+            date: today,
+        });
+    });
+
+    test('should delete a category successfully', (done) => {
+        
+        request(app)
+            .delete("/api/categories")
+            .send({
+                types: ["work"],
+            })
+            .set(
+                "Cookie",
+                `accessToken=${adminToken};refreshToken=${adminToken}`
+            )
+            .then((response) => {
+                expect(response.status).toBe(200);
+                expect(response.body).toStrictEqual({
+                    data: { message: "Categories deleted", count: 2 },
+                });
+                done();
+            })
+            .catch((err) => done(err));
+    });
+
+    test('should return an error if missing field in body', (done) => {
+        
+        request(app)
+            .delete("/api/categories")
+            .send({
+                
+            })
+            .set(
+                "Cookie",
+                `accessToken=${adminToken};refreshToken=${adminToken}`
+            )
+            .then((response) => {
+                expect(response.status).toBe(400);
+                expect(response.body).toStrictEqual({
+                    error: "types object not inserted" 
+                });
+                done();
+            })
+            .catch((err) => done(err));
+    });
+
+    test('should return an error if there\a an empty string', (done) => {
+        
+        request(app)
+            .delete("/api/categories")
+            .send({
+                types: [""],
+            })
+            .set(
+                "Cookie",
+                `accessToken=${adminToken};refreshToken=${adminToken}`
+            )
+            .then((response) => {
+                expect(response.status).toBe(400);
+                expect(response.body).toStrictEqual({
+                    error:  "at least one of the types in the array is an empty string"
+                });
+                done();
+            })
+            .catch((err) => done(err));
+    });
+
+    test('should return an error if there\a a category to be deleted that is not in the database', (done) => {
+        
+        request(app)
+            .delete("/api/categories")
+            .send({
+                types: ["wrong"],
+            })
+            .set(
+                "Cookie",
+                `accessToken=${adminToken};refreshToken=${adminToken}`
+            )
+            .then((response) => {
+                expect(response.status).toBe(400);
+                expect(response.body).toStrictEqual({
+                    error:  "Category for type 'wrong' not found"
+                });
+                done();
+            })
+            .catch((err) => done(err));
+    });
+
+    test('should return an error if there\a only one category in the database', (done) => {
+        request(app)
+            .delete("/api/categories")
+            .send({
+                types: ["investment"],
+            })
+            .set(
+                "Cookie",
+                `accessToken=${adminToken};refreshToken=${adminToken}`
+            )
+            .then((response) => {
+                expect(response.status).toBe(400);
+                expect(response.body).toStrictEqual({
+                    error: "Only one category remaining in database"
+                });
+                done();
+            })
+            .catch((err) => done(err));
+    });
+
+    test('should return an error of authentication (user token)', (done) => {
+        
+        request(app)
+            .delete("/api/categories")
+            .send({
+                types: ["work"],
+            })
+            .set(
+                "Cookie",
+                `accessToken=${userToken};refreshToken=${userToken}`
+            )
+            .then((response) => {
+                expect(response.status).toBe(401);
+                expect(response.body).toStrictEqual({
+                    error: "Mismatch role"
+                });
+                done();
+            })
+            .catch((err) => done(err));
+    });
+
+    test('should return an error of authentication (missing token)', (done) => {
+        
+        request(app)
+            .delete("/api/categories")
+            .send({
+                types: ["work"],
+            })
+            .then((response) => {
+                expect(response.status).toBe(401);
+                expect(response.body).toStrictEqual({
+                    error: "Unauthorized"
+                });
+                done();
+            })
+            .catch((err) => done(err));
+    });
+
+    afterAll(async () => {
+        await categories.deleteMany();
+        await transactions.deleteMany();
     });
 })
 
 describe("getCategories", () => { 
-    test('Dummy test, change it', () => {
-        expect(true).toBe(true);
+    beforeAll(async () => {
+        await categories.create({
+            type: "investment",
+            color: "blue",
+        });
+        await categories.create({
+            type: "work",
+            color: "red",
+        });
+    });
+
+    test('should return all the categories', (done) => {
+        request(app)
+            .get("/api/categories")
+            .set(
+                "Cookie",
+                `accessToken=${adminToken};refreshToken=${adminToken}`
+            )
+            .then((response) => {
+                expect(response.status).toBe(200);
+                expect(response.body).toEqual({
+                    data: [
+                        {
+                            type: "investment",
+                            color: "blue",
+                        },
+                        {
+                            type: "work",
+                            color: "red",
+                        },
+                    ],
+                });
+                done();
+            })
+            .catch((err) => done(err));
+    });
+
+    test('should return an error of authentication (missing token)', (done) => {
+        request(app)
+            .get("/api/categories")
+            .then((response) => {
+                expect(response.status).toBe(401);
+                expect(response.body).toEqual({
+                    error: "Unauthorized"
+                });
+                done();
+            })
+            .catch((err) => done(err));
+    });
+
+    afterAll(async () => {
+        await categories.deleteMany();
     });
 })
 
